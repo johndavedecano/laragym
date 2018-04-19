43288c0776c64e5cded08a52e14ac2ac32295831
John Dave Decano <johndavedecano@gmail.com>
2018-04-19 08:01:15 +0800

    Added seeders and factories

---
+++ database/factories/PackageFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Package::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/SubscriptionFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Subscription::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
