ec11abd96eaaffc2def291f7b867ed2b3229f3b9
John Dave Decano <johndavedecano@gmail.com>
2018-05-19 02:36:05 +0800

    Code clean up

--- resources/apps/frontend/app/containers/App/index.js
+++ resources/apps/frontend/app/containers/App/index.js
@@ -1,11 +1,3 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
