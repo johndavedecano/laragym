f39438cacd0c479d0a32277cabbd6b3e12ffa163
John Dave Decano <johndavedecano@gmail.com>
2018-05-19 04:15:58 +0800

    Fixed page helmet title

--- resources/apps/frontend/app/configureStore.js
+++ resources/apps/frontend/app/configureStore.js
@@ -9,25 +9,24 @@ import thunk from 'redux-thunk';
 import createReducer from './reducers';
 
 export default function configureStore(initialState = {}, history) {
-  // Create the store with two middlewares
-  // 2. routerMiddleware: Syncs the location/URL path to the state
-  const middlewares = [routerMiddleware(history), thunk];
-
-  const enhancers = [applyMiddleware(...middlewares)];
-
-  // If Redux DevTools Extension is installed use it, otherwise use Redux compose
+  // If Redux DevTools Extension is installed use it, otherwise use
+  // Redux compose
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers =
     process.env.NODE_ENV !== 'production' &&
     typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
       ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-          // TODO Try to remove when `react-router-redux` is out of beta, LOCATION_CHANGE should not be fired more than once after hot reloading
-          // Prevent recomputing reducers for `replaceReducer`
+          // TODO Try to remove when `react-router-redux` is out of beta,
+          // LOCATION_CHANGE should not be fired more than once after hot
+          // reloading. Prevent recomputing reducers for `replaceReducer`
         shouldHotReload: false,
       })
       : compose;
   /* eslint-enable */
+
+  const middlewares = [routerMiddleware(history), thunk];
+  const enhancers = [applyMiddleware(...middlewares)];
 
   const store = createStore(
     createReducer(),
@@ -35,18 +34,15 @@ export default function configureStore(initialState = {}, history) {
     composeEnhancers(...enhancers)
   );
 
-  window.store = store;
-
-  // Extensions
-  store.injectedReducers = {}; // Reducer registry
-
   // Make reducers hot reloadable, see http://mxs.is/googmo
   /* istanbul ignore next */
   if (module.hot) {
     module.hot.accept('./reducers', () => {
-      store.replaceReducer(createReducer(store.injectedReducers));
+      store.replaceReducer(createReducer());
     });
   }
+
+  window.store = store;
 
   return store;
 }
