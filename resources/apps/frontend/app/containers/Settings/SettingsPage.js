edb2edbea5b7c19c9ed339e3ed060279831a0a88
John Dave Decano <johndavedecano@gmail.com>
2018-05-22 22:11:27 +0800

    Implement panel component to settings page

--- resources/apps/frontend/app/containers/Settings/SettingsPage.js
+++ resources/apps/frontend/app/containers/Settings/SettingsPage.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+
+import SaveIcon from '@material-ui/icons/Save';
+import Button from '@material-ui/core/Button';
+
+import {
+  Panel,
+  PanelBody,
+  PanelHeader,
+  PanelFooter,
+  PanelActions,
+} from 'components/Panel/Panel';
+
 import Container from 'components/Layouts/Container';
 
-export default class SettingsPage extends Component {
+export default class InvoicesPage extends Component {
   render() {
-    return <Container>Welcome to dashboard</Container>;
+    return (
+      <Container>
+        <Panel>
+          <PanelHeader title="Manage Invoices" />
+          <PanelBody>sdfsdfsfs</PanelBody>
+          <PanelFooter>
+            <PanelActions>
+              <Button size="large" variant="raised" color="primary">
+                <SaveIcon />
+                Save Settings
+              </Button>
+            </PanelActions>
+          </PanelFooter>
+        </Panel>
+      </Container>
+    );
   }
 }
