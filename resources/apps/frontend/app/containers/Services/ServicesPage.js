fd1c678f90f120ce89d1c0de0019e3dd612cdd40
John Dave Decano <johndavedecano@gmail.com>
2018-05-22 22:11:36 +0800

    Implement panel component to services page

--- resources/apps/frontend/app/containers/Services/ServicesPage.js
+++ resources/apps/frontend/app/containers/Services/ServicesPage.js
@@ -1,8 +1,60 @@
 import React, { Component } from 'react';
+
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
+
+import {
+  Panel,
+  PanelBody,
+  PanelHeader,
+  PanelSearch,
+  PanelFooter
+} from 'components/Panel/Panel';
+
 import Container from 'components/Layouts/Container';
 
 export default class ServicesPage extends Component {
+  state = {
+    query: '',
+  };
+
+  onSearchChange = (event) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
+
+  onSubmitSearch = (event) => {
+    event.preventDefault();
+  };
+
   render() {
-    return <Container>Welcome to dashboard</Container>;
+    return (
+      <Container>
+        <Panel>
+          <PanelHeader title="Manage Services">
+            <Button size="large" variant="raised" color="primary">
+              <AddIcon />
+              Add Service
+            </Button>
+          </PanelHeader>
+          <PanelSearch
+            placeholder="Search..."
+            value={this.state.query}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSubmitSearch}
+          />
+          <PanelBody>sdfsdfsfs</PanelBody>
+          <PanelFooter>Hello</PanelFooter>
+        </Panel>
+      </Container>
+    );
   }
 }
