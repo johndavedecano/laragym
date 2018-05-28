41d01f48319f811880c81e97e21cc41df14c0038
John Dave Decano <johndavedecano@gmail.com>
2018-05-29 05:05:17 +0800

    Updated user reducer action handlers

--- resources/apps/frontend/app/reducers/user-reducer.js
+++ resources/apps/frontend/app/reducers/user-reducer.js
@@ -5,13 +5,16 @@ import * as types from './../constants';
 const initialState = fromJS({
   isLoaded: false,
   isLoading: false,
+  isCreating: false,
+  isUpdating: false,
+  isDeleting: false,
   users: Map(),
   params: Map(),
 });
 
 const Logic = {};
 
-Logic[types.USER_LOAD] = (state, action) => state.set('isLoading', true);
+Logic[types.USER_LOAD] = (state) => state.set('isLoading', true);
 
 Logic[types.USER_LOAD_SUCCESS] = (state, action) => {
   if (action.replace) {
@@ -30,25 +33,26 @@ Logic[types.USER_LOAD_SUCCESS] = (state, action) => {
     .set('users', state.get('users').merge(action.users));
 };
 
-Logic[types.USER_LOAD_FAILED] = (state, action) => state;
+Logic[types.USER_LOAD_FAILED] = (state) =>
+  state.set('isLoading', false).set('isLoaded', false);
 
-Logic[types.USER_CREATE] = (state, action) => state;
+Logic[types.USER_CREATE] = (state) => state.set('isCreating', true);
 
-Logic[types.USER_CREATE_SUCCESS] = (state, action) => state;
+Logic[types.USER_CREATE_SUCCESS] = (state) => state.set('isCreating', false);
 
-Logic[types.USER_CREATE_FAILED] = (state, action) => state;
+Logic[types.USER_CREATE_FAILED] = (state) => state.set('isCreating', false);
 
-Logic[types.USER_UPDATE] = (state, action) => state;
+Logic[types.USER_UPDATE] = (state) => state.set('isUpdating', true);
 
-Logic[types.USER_UPDATE_SUCCESS] = (state, action) => state;
+Logic[types.USER_UPDATE_SUCCESS] = (state) => state.set('isUpdating', true);
 
-Logic[types.USER_UPDATE_FAILED] = (state, action) => state;
+Logic[types.USER_UPDATE_FAILED] = (state) => state.set('isUpdating', true);
 
-Logic[types.USER_DELETE] = (state, action) => state;
+Logic[types.USER_DELETE] = (state) => state.set('isDeleting', true);
 
-Logic[types.USER_DELETE_SUCCESS] = (state, action) => state;
+Logic[types.USER_DELETE_SUCCESS] = (state) => state.set('isDeleting', true);
 
-Logic[types.USER_DELETE_FAILED] = (state, action) => state;
+Logic[types.USER_DELETE_FAILED] = (state) => state.set('isDeleting', true);
 
 Logic[types.USER_LOGGED_OUT] = () => initialState;
 
