181d458ae2d223c1e758e39b1616b140f150057c
John Dave Decano <johndavedecano@gmail.com>
2018-06-01 06:15:57 +0800

    Fixed error not showing up

--- resources/apps/frontend/app/helpers/CustomExceptions.js
+++ resources/apps/frontend/app/helpers/CustomExceptions.js
@@ -1,10 +1,9 @@
 export class ApiError {
-  constructor(response) {
+  constructor({ response }) {
     this.name = 'ApiException';
     this.status = response.status;
     this.message = response.data;
     this.errors = {};
-
     if (this.status === 422) {
       this.errors = response.data.error.errors;
       this.message = response.data.error.message;
