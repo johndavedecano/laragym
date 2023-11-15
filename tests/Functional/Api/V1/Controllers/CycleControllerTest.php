c16b16e8dca69543127b0079f6e8171dc31192bc
R3-da <haddan.reda.pers@gmail.com>
2023-11-16 00:23:05 +0100

    add tearDown function to CycleControllerTest

--- tests/Functional/Api/V1/Controllers/CycleControllerTest.php
+++ tests/Functional/Api/V1/Controllers/CycleControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Functional\Api\V1\Controllers;
 
+use App\Models\User;
 use Hash;
 use App\Functional\Api\V1\Controllers\UserLoginTrait;
 use App\Models\Cycle;
@@ -20,6 +21,15 @@ class CycleControllerTest extends TestCase
         parent::setUp();
 
         $this->login('admin@admin.com', 'password');
+    }
+
+    public function tearDown(): void
+    {
+        $user = User::where('email', 'admin@admin.com');
+        if ($user) {
+            $user->delete();
+        }
+        parent::tearDown();
     }
 
     public function testIndexFails()
