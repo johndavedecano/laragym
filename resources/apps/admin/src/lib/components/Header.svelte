0e1a590e506f830d39135b40141952b3622afa5e
John Dave Decano <johndavedecano@gmail.com>
2024-05-02 22:46:52 +0200

    initial layout

---
+++ resources/apps/admin/src/lib/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { AppBar, getDrawerStore } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+
+	const drawerOpen = () => drawerStore.open({});
+</script>
+
+<AppBar background="bg-blue-950">
+	<svelte:fragment slot="lead">
+		<div class="flex items-center to-blue-950">
+			<button class="btn btn-sm mr-4 lg:hidden" on:click={drawerOpen}>
+				<span>
+					<svg viewBox="0 0 100 80" class="h-4 w-4 fill-white">
+						<rect width="100" height="20" />
+						<rect y="30" width="100" height="20" />
+						<rect y="60" width="100" height="20" />
+					</svg>
+				</span>
+			</button>
+			<strong class="text-xl text-white">laragym</strong>
+		</div>
+	</svelte:fragment>
+</AppBar>
--- resources/apps/admin/src/lib/components/Navigation.svelte
+++ resources/apps/admin/src/lib/components/Navigation.svelte
@@ -1,4 +1,16 @@
 <script>
+	import Dashboard from 'svelte-icons/fa/FaHome.svelte';
+	import Activities from 'svelte-icons/fa/FaUserClock.svelte';
+	import Attendance from 'svelte-icons/fa/FaCalendar.svelte';
+	import Members from 'svelte-icons/fa/FaUsers.svelte';
+	import Subscriptions from 'svelte-icons/fa/FaCreditCard.svelte';
+	import Packages from 'svelte-icons/fa/FaBox.svelte';
+	import Services from 'svelte-icons/fa/FaServer.svelte';
+	import Cycles from 'svelte-icons/fa/FaCalendarPlus.svelte';
+	import Settings from 'svelte-icons/fa/FaCog.svelte';
+	import Account from 'svelte-icons/fa/FaUserCog.svelte';
+	import Logout from 'svelte-icons/fa/FaSignOutAlt.svelte';
+
 	import { getDrawerStore } from '@skeletonlabs/skeleton';
 
 	const drawerStore = getDrawerStore();
@@ -6,6 +18,73 @@
 	const drawerClose = () => drawerStore.close();
 </script>
 
-<nav class="list-nav p-4">
-	<slot />
+<nav class="list-nav w-72 p-4">
+	<ul>
+		<li>
+			<a href="/" on:click={drawerClose}
+				><span class="menu-icon"><Dashboard /></span>Dashboard</a
+			>
+		</li>
+		<li>
+			<a href="/activities" on:click={drawerClose}
+				><span class="menu-icon"><Activities /></span>Activities</a
+			>
+		</li>
+		<li>
+			<a href="/attendance" on:click={drawerClose}
+				><span class="menu-icon"><Attendance /></span>Attendance</a
+			>
+		</li>
+		<li>
+			<a href="/members" on:click={drawerClose}
+				><span class="menu-icon"><Members /></span>Members</a
+			>
+		</li>
+		<li>
+			<a href="/subscriptions" on:click={drawerClose}
+				><span class="menu-icon"><Subscriptions /></span>Subscriptions</a
+			>
+		</li>
+		<li>
+			<a href="/packages" on:click={drawerClose}
+				><span class="menu-icon"><Packages /></span>Packages</a
+			>
+		</li>
+		<li>
+			<a href="/services" on:click={drawerClose}
+				><span class="menu-icon"><Services /></span>Services</a
+			>
+		</li>
+		<li>
+			<a href="/cycles" on:click={drawerClose}
+				><span class="menu-icon"><Cycles /></span>Cycles</a
+			>
+		</li>
+		<li>
+			<a href="/account" on:click={drawerClose}
+				><span class="menu-icon"><Account /></span>Account</a
+			>
+		</li>
+		<li>
+			<a href="/settings" on:click={drawerClose}
+				><span class="menu-icon"><Settings /></span>Settings</a
+			>
+		</li>
+		<li>
+			<a href="/logout" on:click={drawerClose}
+				><span class="menu-icon"><Logout /></span>Logout</a
+			>
+		</li>
+	</ul>
 </nav>
+
+<style>
+	a {
+		@apply inline-flex w-full gap-4 text-sm;
+	}
+	.menu-icon {
+		@apply text-white;
+		height: 16px;
+		width: 16px;
+	}
+</style>
