44a297059c039a32cd57f73b4d28190adf7bde0d
John Dave Decano <johndavedecano@gmail.com>
2024-05-04 23:45:47 +0200

    added select components

---
+++ resources/apps/admin/src/lib/components/BranchSelect.svelte
@@ -0,0 +1,36 @@
+<script>
+	// @ts-nocheck
+	import Select from 'svelte-select';
+
+	import { getBearerToken, useApi } from '$lib/api';
+	import { browser } from '$app/environment';
+
+	export let value = [];
+
+	const api = useApi({
+		Authorization: getBearerToken()
+	});
+
+	const loadOptions = async (value) => {
+		try {
+			if (!browser) return [];
+
+			const params = {};
+
+			let options = [];
+
+			if (value) params['filter[name]'] = value;
+
+			const response = await api.get('/branches', { params });
+
+			options = response.data.data;
+
+			return options;
+		} catch (error) {
+			console.error(error);
+			return [];
+		}
+	};
+</script>
+
+<Select {loadOptions} searchable multiple itemId="id" label="name" bind:value {...$$props} />
