a6d851da9b24011571b8e6b566e81303b4673d67
ENGAyman2005 <156946763+ENGAyman2005@users.noreply.github.com>
2025-08-23 19:19:48 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
+//notr
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
